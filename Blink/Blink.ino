63b669f46156dd4c53568e76e0d207ba0e31b806
plogian <lillianpodlog@gmail.com>
2018-01-13 16:54:52 -0500

    initial commit

---
+++ Blink/Blink.ino
@@ -0,0 +1,17 @@
+int led = 9;
+
+void setup() {
+  // put your setup code here, to run once:
+  //initialize the digital pin as output.
+  //pinMode takes two arguments -> the number of the pin and the mode
+  pinMode(led, OUTPUT);
+}
+
+//a void loop does not return anything
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH); //turn the LED on (HIGH is the voltage level)
+  delay(1000);            //wait one second
+  digitalWrite(led, LOW); //turn the LED off
+  delay(1000);            //wait one second
+}
---
+++ Fading_LED_sketch/Fading_LED_sketch.ino
@@ -0,0 +1,27 @@
+int led =9;
+int brightness = 0;
+int fadeAmount = 5;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //digital write can only output a high or low value, but analogWrite
+  //can output a value from 0 to 255. This  corresponds to the duty cycle,
+  //which is the ratio of time that output is high:low
+  //100% of the time high is 255.
+  //50% of the time high is 177. 
+  //Two ways to control brightness:
+  //1. Control voltage, but Arduino Uno has no way of controlling voltage at a pin
+  //2. PWM: Pulse Width Modulation: a technique that allows us to control the
+  //amount of time a signal is at HIGH (5V) or LOW (GND, 0V). 
+  analogWrite(led, brightness);
+  brightness = brightness + fadeAmount;
+  //double equals tests for equality
+  if(brightness == 255 || brightness == 255) 
+    { fadeAmount = -fadeAmount ; }
+    delay(20);
+}
